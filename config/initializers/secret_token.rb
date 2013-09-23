835bcb361c20c63e1ab0d14147d18a4064810d8c
Benjamin Weaver <bweavgolfanatic@aol.com>
2013-09-23 18:02:51 -0400

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Werewolf::Application.config.secret_token = 'e15b69690f67c76cbbd17423dcddf2f7dc6237bb275e847dd325c36d07a20ff8ec48618214a062d5e7c02ec81c0981e535d0ce58200e97aa2149115a907fdcc3'
