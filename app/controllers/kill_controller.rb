cef972b04711a85ba9e9b8453c6c1187e528efea
Benjamin Weaver <bweavgolfanatic@aol.com>
2013-10-07 12:13:55 -0400

    deleted old stuff

--- app/controllers/kill_controller.rb
+++
@@ -1,2 +0,0 @@
-class KillController < ApplicationController
-end
--- db/migrate/20130927025324_add_column_to_game.rb
+++
@@ -1,5 +0,0 @@
-class AddColumnToGame < ActiveRecord::Migration
-  def change
-    add_column :games, :day_state, :string
-  end
-end
