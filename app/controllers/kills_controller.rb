7390f7eb7c147c5001ba01e2eb637dfc0f624fbc
Benjamin Weaver <bweavgolfanatic@aol.com>
2013-12-04 02:47:28 -0500

    daily report

--- app/controllers/kills_controller.rb
+++ app/controllers/kills_controller.rb
@@ -4,9 +4,11 @@ class KillsController < ApplicationController
     kills_hash = Hash.new
     if Game.all.length != 0
       if (((Time.now - Game.last.created_at) % (120*Game.last.dayNightFreq)) < (Game.last.dayNightFreq*60))
-        Kill.all.each do |kill|
+        @kills = Kill.all
+        i = 0
+        while i < @kills.length
           if Time.now - kill.created_at < 120*Game.last.dayNightFreq
-            kills_hash[kill.victimID] = kill.created_at + ": " + kill.lat + ",  " + kill.lng
+            kills_hash[i] = Player.find_by_user_id(kill.victimID).nickname + kill.created_at.to_s + ": " + kill.lat.to_s + ",  " + kill.lng.to_s
           end
         end
       end
